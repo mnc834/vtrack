f80e015fd285ea15c4f57519c5c41165479e5c1d
Nikolay Chuprov <pkq874@motorolasolutions.com>
2016-03-30 18:28:45 +0300

    rel skeleton

---
+++ Makefile
@@ -0,0 +1,8 @@
+PROJECT = vtrack
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.0.1
+
+# Whitespace to be used when creating files from templates.
+SP = 2
+
+include erlang.mk
---
+++ src/vtrack_app.erl
@@ -0,0 +1,11 @@
+-module(vtrack_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+  vtrack_sup:start_link().
+
+stop(_State) ->
+  ok.
